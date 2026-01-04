654bb946aaa08d506af36ed58adc6eea4d6ff8ac
ayusha1223 <ayushathapa425@gmail.com>
2026-01-04 15:24:54 +0545

    User Model Added

---
+++ src/models/user.model.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+export const User = model("User", userSchema);
