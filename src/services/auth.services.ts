6d21e86a399e56f0dde1fd9ce490c6c7c7848681
ayusha1223 <ayushathapa425@gmail.com>
2026-01-04 15:26:23 +0545

    Added Business Logic

---
+++ src/services/auth.services.ts
@@ -0,0 +1,32 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { AuthRepository } from "../repositories/auth.repository";
+
+export class AuthService {
+  static async register(data: any) {
+    const existingUser = await AuthRepository.findByEmail(data.email);
+    if (existingUser) throw new Error("Email already exists");
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return AuthRepository.createUser({
+      ...data,
+      password: hashedPassword,
+    });
+  }
+
+  static async login(data: any) {
+    const user = await AuthRepository.findByEmail(data.email);
+    if (!user) throw new Error("Invalid credentials");
+
+    const isMatch = await bcrypt.compare(data.password, user.password);
+    if (!isMatch) throw new Error("Invalid credentials");
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET as string
+    );
+
+    return { token };
+  }
+}
