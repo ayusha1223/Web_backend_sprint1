eceebd1f4f65c23d93e6ab00b237f6b7ec1dbffc
ayusha1223 <ayushathapa425@gmail.com>
2026-01-04 16:20:27 +0545

    .env

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+  console.log("MONGO_URI =", process.env.MONGO_URI); // 👈 ADD THIS
+
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log("Database connected");
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route";
 import { connectDB } from "./database";
+import authRoutes from "./routes/auth.route";
 
-dotenv.config();
+dotenv.config();        // 🔴 MUST be before connectDB
 connectDB();
 
 const app = express();
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
