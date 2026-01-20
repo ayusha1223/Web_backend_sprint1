44fcc5afd0219be2a39046bbb9e88d76e25dc54a
ayusha1223 <ayushathapa425@gmail.com>
2026-01-20 09:59:36 +0545

    auth.comtroller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,15 +2,14 @@ import { Request, Response } from "express";
 import { registerDto, loginDto } from "../dtos/auth.dto";
 import { AuthService } from "../services/auth.services";
 
-
 export class AuthController {
   static async register(req: Request, res: Response) {
     try {
       const data = registerDto.parse(req.body);
       const user = await AuthService.register(data);
-      res.status(201).json(user);
+      return res.status(201).json(user);
     } catch (err: any) {
-      res.status(400).json({ message: err.message });
+      return res.status(400).json({ message: err.message });
     }
   }
 
@@ -18,9 +17,9 @@ export class AuthController {
     try {
       const data = loginDto.parse(req.body);
       const result = await AuthService.login(data);
-      res.json(result);
+      return res.json(result);
     } catch (err: any) {
-      res.status(400).json({ message: err.message });
+      return res.status(400).json({ message: err.message });
     }
   }
 }
