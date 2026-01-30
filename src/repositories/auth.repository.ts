ff1851a766e08797eec2796a774ed7db746befde
ayusha1223 <ayushathapa425@gmail.com>
2026-01-30 15:40:32 +0545

    upload middlware

--- src/repositories/auth.repository.ts
+++ src/repositories/auth.repository.ts
@@ -1,6 +1,9 @@
 import { User } from "../models/user.model";
 
 export class AuthRepository {
+  findUserByEmail(email: any) {
+    throw new Error("Method not implemented.");
+  }
   static findByEmail(email: string) {
     return User.findOne({ email });
   }
